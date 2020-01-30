a6745467746faed0e1facc0917668f88c0231ed0
imane <benbenimane@hotmail.com>
2020-01-30 16:03:36 +0100

    adding transitions to change from section to section smoothly

--- script.js
+++ script.js
@@ -5,9 +5,22 @@ const game = ()=> {
   let playerScore = 0;
   let computerScore = 0;
 
+const startGame = () =>{
+const playBtn = document.querySelector('.intro button');
+ const introScreen = document.querySelector('.intro');
+ const match = document.querySelector('.match');
 
+ playBtn.addEventListener('click', () =>{
 
+     introScreen.classList.add("fadeOut");
+     match.classList.add("fadeIn");
+    });
+};
 
+//call all inner functioons
+startGame();
 
+};
 
-}
+//start game function
+game();
